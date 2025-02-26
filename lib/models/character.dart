5a75e0e35df7e730ca5df33db6edcc291fc53298
l4mole <danielrojo9822@gmail.com>
2025-02-26 13:03:54 -0600

    Agregar modelos Character y Question para estructurar los datos del cuestionario

---
+++ lib/models/character.dart
@@ -0,0 +1,11 @@
+class Character {
+  final String name;
+  final String descripction;
+  final String imagePath;
+
+  const Character({
+    required this.name,
+    required this.descripction,
+    required this.imagePath,
+  });
+}
---
+++ lib/models/question.dart
@@ -0,0 +1,7 @@
+class Question {
+  final String text;
+  final List<String> options;
+  final Map<String, int> scores;
+
+  Question({required this.text, required this.options, required this.scores});
+}
